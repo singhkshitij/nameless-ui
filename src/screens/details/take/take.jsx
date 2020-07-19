3582aa73d9607e3fdb2a793bdf51ae0ac6115c01
kshitij.singh <kshitij.singh@go-jek.com>
2020-07-19 14:25:13 +0530

    Sends joining person name

--- src/screens/details/take/take.jsx
+++ src/screens/details/take/take.jsx
@@ -1,38 +1,48 @@
-import React, { Component } from 'react';
-import TextField, {HelperText, Input} from '@material/react-text-field';
-import '@material/react-text-field/dist/text-field.css';
-import Button from '../../../components/button/button';
-import { Link } from 'react-router-dom';
-import { FcPortraitMode } from 'react-icons/fc';
-import { MdClearAll } from 'react-icons/md';
-import { nanoid } from 'nanoid'
-import './take.css'
+import React, { Component } from "react";
+import TextField, { HelperText, Input } from "@material/react-text-field";
+import "@material/react-text-field/dist/text-field.css";
+import Button from "../../../components/button/button";
+import { Link } from "react-router-dom";
+import { FcPortraitMode } from "react-icons/fc";
+import { MdClearAll } from "react-icons/md";
+import { nanoid } from "nanoid";
+import "./take.css";
 
 export default class Take extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hostName: "",
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-          hostName: ""
-        };
-      }
-
-    render() {
-        return (
-            <div className="details-action">
-                <TextField
-                label='Host name'
-                helperText={<HelperText>Be original!</HelperText>}
-                onTrailingIconSelect={() => this.setState({hostName: ''})}
-                leadingIcon={<FcPortraitMode/>}
-                trailingIcon={<MdClearAll/>}
-                outlined>
-                    <Input value={this.state.hostName} onChange={(e) => this.setState({hostName: e.currentTarget.value})} />
-                </TextField>
-                <Link to={"/room/" + nanoid(10)}>
-                      <Button text="Create room"/>
-                </Link>
-            </div>  
-        );
-      }
+  render() {
+    return (
+      <div className="details-action">
+        <TextField
+          label="Host name"
+          helperText={<HelperText>Be original!</HelperText>}
+          onTrailingIconSelect={() => this.setState({ hostName: "" })}
+          leadingIcon={<FcPortraitMode />}
+          trailingIcon={<MdClearAll />}
+          outlined
+        >
+          <Input
+            value={this.state.hostName}
+            onChange={(e) => this.setState({ hostName: e.currentTarget.value })}
+          />
+        </TextField>
+        <Link
+          to={{
+            pathname: "/room/" + nanoid(10),
+            state: {
+              name: this.state.hostName,
+            },
+          }}
+        >
+          <Button text="Create room" />
+        </Link>
+      </div>
+    );
+  }
 }
--- src/screens/room/room.jsx
+++ src/screens/room/room.jsx
@@ -8,54 +8,12 @@ export default class Room extends Component {
 
   constructor(props) {
     super(props);
-    this.sendMessage = this.sendMessage.bind(this);
+    this.addMessage = this.addMessage.bind(this);
     this.state = {
       uid : this.props.match.params.url || "abc",
-      name : "Kshitij",
-      data : [{
-        "type":"message",
-        "data":"Hello from Kshitij",
-        "owner":"me",
-        "dt":"12:33 PM"
-      },{
-        "type":"message",
-        "data":"Hello from Rahul",
-        "owner":"Rahul",
-        "dt":"12:33 PM"
-      },{
-        "type":"entry",
-        "data":"Kshitij joined the chat",
-        "owner":"me",
-        "dt":"12:33 PM"
-      },{
-        "type":"entry",
-        "data":"Rahul joined the chat",
-        "owner":"Rahul",
-        "dt":"12:33 PM"
-      }]
+      name : this.props.location.state.name,
+      data : []
     };
-  }
-
-  componentDidMount() {
-      const ws = new WebSocket('ws://localhost:8080/' + this.state.uid +"/" + this.state.name)
-      ws.onopen = () => {
-      // on connecting, do nothing but log it to the console
-      console.log('connected')
-      }
-
-      ws.onmessage = evt => {
-      // listen to data sent from the websocket server
-      const message = JSON.parse(evt.data)
-      console.log(this.state.data)
-      this.setState({data: this.state.data.concat(message)})
-      console.log(this.state.data)
-      }
-
-      ws.onclose = () => {
-      console.log('disconnected')
-      // automatically try to reconnect on connection loss
-
-      }
   }
 
   formatAMPM() {
@@ -69,23 +27,40 @@ export default class Room extends Component {
     var strTime = hours + ':' + minutes + ' ' + ampm;
     return strTime;
   }
-  
 
-  async sendMessage(message){
+  async addMessage(type,message){
 
     let details = {
-      "type":"message",
+      "type": type,
       "data": message,
       "owner":"me",
       "dt": this.formatAMPM()
     }
-    let messages = [details, ...this.state.data]
 
+    let messages = [details, ...this.state.data]
     await this.setState({
       data: messages
     })
-    
-    console.log(this.state.data);
+  }
+
+  componentDidMount() {
+      const ws = new WebSocket('ws://localhost:8080/' + this.state.uid +"?" + this.state.name)
+      ws.onopen = () => {
+        // on connecting, do nothing but log it to the console
+        console.log('Connection established !');
+      }
+
+      ws.onmessage = evt => {
+          // listen to data sent from the websocket server
+          const message = JSON.parse(evt.data)
+          this.addMessage(message.type,message.message)
+      }
+
+      ws.onclose = () => {
+      console.log('disconnected')
+      // automatically try to reconnect on connection loss
+
+      }
   }
 
   lpStyle = {
@@ -104,7 +79,7 @@ export default class Room extends Component {
       <div className="room" style={this.lpStyle}>
         <Header chatPage hostName="kshitij"/>
         <ChatContent data={this.state.data}/>
-        <MessageBox messageHook={this.sendMessage}/>
+        <MessageBox messageHook={this.addMessage}/>
       </div>
     );
   }
