a72bcc7dd4f9c81a609cd9c79568e793d7ba4814
kshitij.singh <kshitij.singh@go-jek.com>
2020-07-30 20:12:47 +0530

    Changed hostname

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import LandingPage from "./screens/landingPage/landingPage";
 import DetailsPage from "./screens/details/details";
 import Room from "./screens/room/room";
 import { Route, Switch } from "react-router-dom";
-import ErrorBoundary from "./components/errorBoundary/errorBoundary";
 
 const cacheImages = (imgs) => {
   imgs.forEach((src) => {
