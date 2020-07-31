59191195b0fd07c96ed59d89be1d414ca18d8d66
kshitij.singh <kshitij.singh@go-jek.com>
2020-07-31 17:20:40 +0530

    Debug ws

--- src/screens/room/room.jsx
+++ src/screens/room/room.jsx
@@ -4,104 +4,98 @@ import ChatContent from "./chatContent/chatContent";
 import MessageBox from "./messageBox/messageBox";
 import Constants from "../../constants";
 import axios from "axios";
-import './room.css'
+import "./room.css";
 
 export default class Room extends Component {
-
   constructor(props) {
     super(props);
     this.sendMessage = this.sendMessage.bind(this);
     this.state = {
-      uid : this.props.match.params.url || "abc",
-      name : this.props.location.state.name,
-      data : [],
-      error:""
+      uid: this.props.match.params.url || "abc",
+      name: this.props.location.state.name,
+      data: [],
+      error: "",
     };
   }
 
   ws = null;
 
-  async sendMessage(type,message){
-
+  async sendMessage(type, message) {
     let details = {
-      "url": this.state.uid,
-      "data": message,
-      "owner": this.state.name,
-    }
+      url: this.state.uid,
+      data: message,
+      owner: this.state.name,
+    };
 
-    this.ws.send(JSON.stringify(details))
-
+    this.ws.send(JSON.stringify(details));
   }
 
-  async addMessage(type,message){
-
+  async addMessage(type, message) {
     let details = {
-      "type": type,
-      "data": message.data,
-      "owner": message.owner,
-      "dt": message.dt
-    }
+      type: type,
+      data: message.data,
+      owner: message.owner,
+      dt: message.dt,
+    };
 
-    let messages = [details, ...this.state.data]
+    let messages = [details, ...this.state.data];
     await this.setState({
-      data: messages
-    })
+      data: messages,
+    });
   }
-  
-  establishWsConnection(){
-      console.log("WS connection getting established");
-      let host = Constants.wsHost;
-      const url = process.env[host] + "/" + this.state.uid +"?" + this.state.name;
-      this.ws = new WebSocket(url)
-      
-      this.ws.onopen = () => {
-        console.log('WS connection established !');
-      }
 
-      this.ws.onmessage = evt => {
-          const message = JSON.parse(evt.data)
-          this.addMessage(message.type,message.message)
-      }
+  establishWsConnection() {
+    let host = Constants.wsHost;
+    const url =
+      process.env[host] + "/" + this.state.uid + "?" + this.state.name;
 
-      this.ws.onclose = () => {
-      console.log('disconnected')
+    console.log("Host ", host, url)  
+    this.ws = new WebSocket(url);
+
+    this.ws.onopen = () => {
+      console.log("WS connection established !");
+    };
+
+    this.ws.onmessage = (evt) => {
+      const message = JSON.parse(evt.data);
+      this.addMessage(message.type, message.message);
+    };
+
+    this.ws.onclose = () => {
+      console.log("disconnected");
       // automatically try to reconnect on connection loss
-
-      }
+    };
   }
 
   getChatHistory = async () => {
-
-    console.log("inside chat history");
     let host = Constants.serverHostKey;
     const url = process.env[host] + "/api/v1/chats/" + this.state.uid;
-   
+
     await axios
-      .get( url)
+      .get(url)
       .then((res) => {
         if (res.data.data) {
-          this.setState({ data: res.data.data});
+          this.setState({ data: res.data.data });
           this.establishWsConnection();
         } else {
           this.setState({ error: "😕 Failed to get chat history !" });
         }
       })
-      .catch(error => {
-        this.setState({ error: "😕 Failed to get chat history !"});
-    });;
-    console.log("coming out of chat history");
-  }
+      .catch((error) => {
+        this.setState({ error: "😕 Failed to get chat history !" });
+      });
+  };
 
   componentDidMount() {
-      this.getChatHistory();
+    this.getChatHistory();
   }
 
   render() {
     return (
       <div className="room">
-        <Header chatPage hostName={this.state.name}/>
-        <ChatContent data={this.state.data} owner={this.state.name}/>
-        <MessageBox messageHook={this.sendMessage}/>
+        <Header chatPage hostName={this.state.name} />
+        <ChatContent data={this.state.data} owner={this.state.name} />
+        <MessageBox messageHook={this.sendMessage} />
       </div>
     );
   }
