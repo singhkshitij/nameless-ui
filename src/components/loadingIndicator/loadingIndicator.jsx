18bcb1bdc4fc4137224e474e9aff2f4cad8eeda2
kshitij.singh <kshitij.singh@go-jek.com>
2020-08-03 13:52:40 +0530

    Adds loading indicator

--- src/components/loadingIndicator/loadingIndicator.jsx
+++ src/components/loadingIndicator/loadingIndicator.jsx
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
 import { css } from "@emotion/core";
 import GridLoader from "react-spinners/GridLoader";
+import PuffLoader from "react-spinners/PuffLoader";
 import "./loadingIndicator.css";
 
 export default class LoadingIndicator extends Component {
-
   render() {
     return (
       <div className="loading-indicator">
-        <GridLoader
-          css={css}
-          size={10}
-          color={"#6f84ef"}
-          loading
-        />
+        {this.props.round ? (
+          <PuffLoader css={css} size={40} color={"#6f84ef"} loading />
+        ) : (
+          <GridLoader css={css} size={10} color={"#6f84ef"} loading />
+        )}
       </div>
     );
   }
--- src/screens/room/chatContent/chatContent.jsx
+++ src/screens/room/chatContent/chatContent.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import Bubble from "../../../components/bubble/bubble";
+import LoadingIndicator from "../../../components/loadingIndicator/loadingIndicator";
 import "./chatContent.css";
 
 export default class ChatContent extends Component {
   render() {
     return (
       <div className="chat-content">
-         <Bubble data={this.props.data} owner={this.props.owner}/>
+        {this.props.data.length ? (
+          <div style={{ marginTop: "10px" }}>
+            <LoadingIndicator round/>
+          </div>
+        ) : (
+          <Bubble data={this.props.data} owner={this.props.owner} />
+        )}
       </div>
     );
   }
