557b2a0ff2c45dcf699b2810a7bf7398a143735c
kshitij.singh <kshitij.singh@go-jek.com>
2020-08-22 15:33:25 +0530

    Added roomtour feature

---
+++ src/urls.jsx
@@ -0,0 +1,28 @@
+import Constants from "./constants";
+
+function getHost() {
+  let host = Constants.serverHostKey;
+  return process.env[host];
+}
+
+function getWSHost() {
+  let host = Constants.wsHost;
+  return process.env[host];
+}
+
+const ServerUrls = {
+  checkIfRoomExists: function (roomId) {
+    return getHost() + "/api/v1/active/room/" + roomId;
+  },
+  createRoom: function (uid) {
+    return getHost() + "/api/v1/room/" + uid;
+  },
+  getChatHistory: function (uid) {
+    return getHost() + "/api/v1/chats/" + uid;
+  },
+  getWSconnection: function (uid, name) {
+    return getWSHost() + "/" + uid + "?" + name;
+  },
+};
+
+export default ServerUrls;
